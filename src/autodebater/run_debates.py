386e7200b1e170b2c1d5fdd7ab7f6a5989a3bccf
Neal Lewis <imneal@gmail.com>
2024-06-10 09:41:31 +0100

    feat: pretty print cli

--- src/autodebater/run_debates.py
+++ src/autodebater/run_debates.py
@@ -3,48 +3,51 @@ CLI entry point for debates
 """
 
 import typer
+from rich.console import Console
+from rich.live import Live
+from rich.table import Table
 
-from autodebater.debate import JudgedDebate, SimpleDebate
-from autodebater.participants import BullshitDetector, Debater, Judge
+from autodebater.debate_runners import (BasicJudgedDebateRunner,
+                                        BasicSimpleDebateRunner)
+from autodebater.dialogue import DialogueMessage
 
 app = typer.Typer()
+
+
+def msg2table(msg: DialogueMessage):
+    table = Table("name", "role", "message")
+    table.add_row(msg.name, msg.role, msg.message)
+    return table
 
 
 @app.command()
 def judged_debate(motion: str, epochs: int = 2):
     """Start a new debate with the given motion and epochs."""
-    debate = JudgedDebate(motion=motion, epochs=epochs)
-
-    debater1 = Debater(name="Debater1", motion=motion, stance="for")
-    debater2 = Debater(name="Debater2", motion=motion, stance="against")
-    judge = Judge(name="Judge", motion=motion)
-    bullshit_detector = BullshitDetector(name="BullshitDetector", motion=motion)
-    # Add debaters to the debate
-    debate.add_debaters(debater1)
-    debate.add_debaters(debater2)
-    debate.add_judge(judge)
-    debate.add_judge(bullshit_detector)
+    debate_runner = BasicJudgedDebateRunner(motion=motion, epochs=epochs)
 
     typer.echo(f"Starting debate on: {motion}")
-    for msg in debate.debate():
-        typer.echo(f"{msg.timestamp} - {msg.name} ({msg.role}): {msg.message}")
+    console = Console()
+
+    table = Table("name", "role", "message")
+    with Live(table, refresh_per_second=4):
+        for msg in debate_runner.run_debate():
+            table.add_row(msg.name, msg.role, msg.message)
+        # console.print(table)
+
+        # typer.echo(f"{msg.timestamp} - {msg.name} ({msg.role}): {msg.message}")
 
 
 @app.command()
 def simple_debate(motion: str, epochs: int = 2):
     """Start a new debate with the given motion and epochs."""
-    debate = SimpleDebate(motion=motion, epochs=epochs)
-
-    debater1 = Debater(name="Debater1", motion=motion, stance="for")
-    debater2 = Debater(name="Debater2", motion=motion, stance="against")
-
-    # Add debaters to the debate
-    debate.add_debaters(debater1)
-    debate.add_debaters(debater2)
+    debate_runner = BasicSimpleDebateRunner(motion=motion, epochs=epochs)
 
     typer.echo(f"Starting debate on: {motion}")
-    for msg in debate.debate():
-        typer.echo(f"{msg.timestamp} - {msg.name} ({msg.role}): {msg.message}")
+    console = Console()
+
+    for msg in debate_runner.run_debate():
+        table = msg2table(msg)
+        console.print(table)
 
 
 if __name__ == "__main__":
