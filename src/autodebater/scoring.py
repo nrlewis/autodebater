e78837871140d841b25bb9f83d8abbb8ddbad270
Neal Lewis <imneal@gmail.com>
2024-06-18 06:55:10 +0100

    fix: remove numpy

--- src/autodebater/scoring.py
+++ src/autodebater/scoring.py
@@ -2,11 +2,12 @@
 Helper functions for determining the scoring
 """
 
-import numpy as np
-
 
 def geometric_mean(scores):
     adjusted_scores = [
         max(score, 1e-10) for score in scores
     ]  # Replace zero scores with a small positive value
-    return np.prod(adjusted_scores) ** (1.0 / len(adjusted_scores))
+    product = 1
+    for score in adjusted_scores:
+        product *= score
+    return product ** (1.0 / len(adjusted_scores))
